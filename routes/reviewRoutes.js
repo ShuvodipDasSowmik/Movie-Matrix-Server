c5fa166b8917fbbaee8feda73b1414a73f579c78
ShuvodipDasSowmik <shuvodipsowmik@gmail.com>
2025-07-21 17:26:46 +0600

    Reviews + Fine Tuning

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -7,4 +7,9 @@ router.post('/create-review', verifyToken, reviewController.createMovieReview);
 router.put('/update-review/:reviewid', verifyToken, reviewController.updateMovieReview);
 router.delete('/delete-review/:reviewid', verifyToken, reviewController.deleteMovieReview);
 
+router.get('/episode-reviews/:seasonid', reviewController.getEpisodeReviewByID);
+router.post('/create-episode-review', verifyToken, reviewController.createEpisodeReview);
+router.put('/update-episode-review', verifyToken, reviewController.updateEpisodeReview);
+router.delete('/delete-episode-review', verifyToken, reviewController.deleteEpisodeReview);
+
 module.exports = router;
